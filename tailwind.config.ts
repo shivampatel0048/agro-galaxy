2e8039dbb48a9c140276fa63bb427734b52e0478
Shivam Patel <shivampatel0048@gmail.com>
2024-11-19 00:52:08 +0530

    initial setup commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,29 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	darkMode: ["class"],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)'
+			},
+			borderRadius: {
+				lg: '0.5rem',
+				md: '0.375rem',
+				sm: '0.125rem'
+			},
+			screens: {
+				'3xl': '1920px',
+				'4xl': '2560px',
+			}
+		}
+	},
+	plugins: [tailwindcssAnimate],
 } satisfies Config;
