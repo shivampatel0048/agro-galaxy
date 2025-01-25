3048861bea530e501d1122e4d8b682c79e327a79
Shivam Patel <shivampatel0048@gmail.com>
2025-01-25 20:22:34 +0530

    feat: update API base URL to use environment variable for better configuration

--- utils/config.ts
+++ utils/config.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 import { getToken } from "./tokenUtils";
 
 const API = axios.create({
-  baseURL: `http://localhost:5000`,
-  // baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
   headers: {
     "Content-Type": "application/json",
   },
